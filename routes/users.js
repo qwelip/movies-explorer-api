1726fdfe4f229086a41dc248934efd7e03e115d5
AndMed <andmed20@yandex.ru>
2022-06-19 15:08:49 +0300

    Revert "init commit"
    This reverts commit ea0c47f55a683bb2e7737cbc5d2b21908963bf42.

--- routes/users.js
+++
@@ -1,7 +0,0 @@
-const usersRouter = require('express').Router();
-const { getCurrentUser, updateCurrentUser } = require('../controllers/users');
-
-usersRouter.get('/me', getCurrentUser);
-usersRouter.post('/me', updateCurrentUser);
-
-module.exports = usersRouter;
